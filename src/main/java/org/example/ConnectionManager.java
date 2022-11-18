b5b8a52be29b2af142d054f2b60e248991ca712b
김재령 <wofud0321@naver.com>
2022-11-18 22:56:52 +0900

    11/18 Test

---
+++ src/main/java/org/example/ConnectionManager.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_URL = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    private static final int MAX_POOL_SIZE = 40;
+    private static final DataSource ds;
+
+    // 데이터소스는 하나여야하기 때문에 static으로 초기화해준다.
+    static {
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setDriverClassName(DB_DRIVER);
+        hikariDataSource.setJdbcUrl(DB_URL);
+        hikariDataSource.setUsername("sa");
+        hikariDataSource.setPassword("");
+        hikariDataSource.setMaximumPoolSize(MAX_POOL_SIZE);
+        hikariDataSource.setMinimumIdle(MAX_POOL_SIZE);
+
+        ds = hikariDataSource;
+    }
+
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection(); // 데이터소스로부터 커넥션을 받아온다.
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
